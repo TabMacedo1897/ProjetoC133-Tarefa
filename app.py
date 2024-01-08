5a87ab34d41d23795138bda2eb10856d00cad4a7
m1gu3l022 <tabata.macedo1897@gmail.com>
2024-01-08 11:19:53 -0300

    Atualização do arquivo index.html

--- app.py
+++ app.py
@@ -1,27 +1,29 @@
-# importando os módulos da biblioteca flask
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
-# criando instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+@app.route("/")
+def home():
+    nome = "Kairo"
+    idade = "14"
+    return render_template('index.html', nome=nome, idade=idade)
 
+@app.route("/pai")
+def pagina_pai():
+    nome_pai = "Nome do Pai"
+    return render_template('pai.html', nome_pai=nome_pai)
 
-# escreva as rotas usando funções de decorador
-# rota padrão ou 'URL'
+@app.route("/mae")
+def pagina_mae():
+    nome_mae = "Nome da Mãe"
+    return render_template('mae.html', nome_mae=nome_mae)
 
+@app.route("/amigo")
+def pagina_amigo():
+    nome_amigo = "Nome do Amigo"
+    return render_template('amigo.html', nome_amigo=nome_amigo)
 
-# defina a rota para a página do pai
+# Adicione outras rotas conforme necessário
 
-
-# defina a rota para a página da mãe
-
-
-
-# defina a rota para a página do amigo
-
-
-
-# adicione outras rotas, se você quiser
-
-
-# execute o arquivo
-if __name__  ==  '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
