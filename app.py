e8a4b816144d84f5d21c26ddfa0ae2d1d05139bf
m1gu3l022 <tabata.macedo1897@gmail.com>
2024-01-08 09:57:41 -0300

    Projeto C133

---
+++ app.py
@@ -0,0 +1,27 @@
+# importando os módulos da biblioteca flask
+
+
+# criando instância da classe Flask, fornecendo a palavra-chave __name__ como argumento
+
+
+# escreva as rotas usando funções de decorador
+# rota padrão ou 'URL'
+
+
+# defina a rota para a página do pai
+
+
+# defina a rota para a página da mãe
+
+
+
+# defina a rota para a página do amigo
+
+
+
+# adicione outras rotas, se você quiser
+
+
+# execute o arquivo
+if __name__  ==  '__main__':
+    app.run(debug=True)
